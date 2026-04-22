181e3825e082039472bb671b942c3d5acd136646
vitorvet <vitorvet@me.com>
2026-04-22 17:35:16 -0300

    add api users

--- api/users.js
+++ api/users.js
@@ -1,4 +1,3 @@
-cat > ~/Desktop/bioterio/api/users.js << 'EOF'
 import { createClient } from '@supabase/supabase-js';
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_KEY);
 
@@ -31,4 +30,3 @@ export default async function handler(req, res) {
 
   return res.status(405).json({ error: 'Método não permitido.' });
 }
-EOF
